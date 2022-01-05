52755c84d6d52202d88f0f22ac78c25e4eda8b2f
jeremiahcanlas <jeremiahcanlas@gmail.com>
2022-01-04 19:08:26 -0500

    added cloudinary for image upload

--- config/plugins.js
+++ config/plugins.js
@@ -3,9 +3,14 @@ module.exports = ({ env }) => ({
   upload: {
     provider: "cloudinary",
     providerOptions: {
-      cloud_name: "jeremiahcanlas",
-      api_key: "627422381757535",
-      api_secret: "DE3JmjCf0dapK1wPhr___dpK2wc",
+      cloud_name: env("CLOUD_NAME"),
+      api_key: env("CLOUDINARY_API_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
+    },
+    actionOptions: {
+      upload: {
+        folder: "ttw",
+      },
     },
   },
   // ...
