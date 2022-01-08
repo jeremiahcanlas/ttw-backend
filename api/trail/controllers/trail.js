8046ddcb379b21eacd0b990da1e6d9ed9cf6f489
jeremiahcanlas <jeremiahcanlas@gmail.com>
2022-01-07 23:39:52 -0500

    added a custom delete controller for trail entry

--- api/trail/controllers/trail.js
+++ api/trail/controllers/trail.js
@@ -11,13 +11,25 @@ module.exports = {
     let entity;
     if (ctx.is("multipart")) {
       const { data, files } = parseMultipartData(ctx);
-
       data.user = ctx.state.user.id;
-
       entity = await strapi.services.trail.create(data, { files });
     } else {
       ctx.request.body.user = ctx.state.user.id;
       entity = await strapi.services.trail.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.trail });
+  },
+  async delete(ctx) {
+    const { id } = ctx.params;
+    const entity = await strapi.services.trail.delete({ id });
+
+    // with strapi multiple media
+    if (entity) {
+      if (entity.images.length >= 1) {
+        entity.images.forEach((image) => {
+          strapi.plugins.upload.services.upload.remove(image);
+        });
+      }
     }
 
     return sanitizeEntity(entity, { model: strapi.models.trail });
